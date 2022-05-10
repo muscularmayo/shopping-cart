843229d818de5c0aebba0b6f8e35ccf7dfd5bc11
Chris Risden <christopherrisden@gmail.com>
2022-05-09 18:00:46 -0700

    still can't navigate straight to /shop/:id but.. in progress.. working on all sorts of little things to make this look niceee

--- src/ExpandedShopItem.js
+++ src/ExpandedShopItem.js
@@ -1,21 +1,38 @@
 import './expandedshopitem.css';
 import { useParams, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import axios from 'axios';
+import { useState } from 'react';
 
 const ExpandedShopItem = (props) => {
   const id = Number(useParams().id)
   console.log(id, useParams())
-  const shopItem = useSelector(state => state.shopData.shopArray[id])
+  const shopItemObject = useSelector(state => state.shopData.shopArray[id])
 
+  const [shopItem, setShopItem] = useState(shopItemObject)
+
+  console.log(shopItem)
   let navigate = useNavigate();
 
+  if (shopItem === undefined) {
+    const fetchedShopItem = undefinedShopItem()
+    setShopItem(fetchedShopItem)
+  }
 
+  async function undefinedShopItem() {
+    const response = await axios.get(`https://fakestoreapi.com/products/${id+1}`)
+    const data = response.data;
+    console.log(response, data)
+    return data;
+  }
+  console.log(shopItem)
 
   //if shopItem is undefined, we should fetch the info, right now we are assuming that we navigated here through /shop/
 
   return (
     <div className="expanded-shop-item modal">
-      <div className="modal-content">
+      {shopItem !== undefined ? (
+        <div className="modal-content">
         <button onClick={() => navigate('/shop')}>Back to Shop</button>
         <img className="expanded-shop-item-image" src={shopItem.img} alt='{props.item.img}'/>
         <div className="expanded-item-title">
@@ -27,13 +44,33 @@ const ExpandedShopItem = (props) => {
         <div className="expanded-item-description">
           {shopItem.description}
         </div>
-        <div className="expanded-item-rating">
+        {/* <div className="expanded-item-rating">
           {shopItem.rating.rate + ' stars with ' + shopItem.rating.count + ' reviews.'}
-        </div>
+        </div> */}
       </div>
-
+      ) : (
+        <p>loading...</p>
+      )}
   </div>
   );
 }
 
 export default ExpandedShopItem;
+
+/*
+
+  return (
+    <div>
+      <h1>Shop</h1>
+      {loading === false ? (
+        <div className="shop-wrapper">
+          <SideBar categories={setSideBarInfo()}/>
+          <Catalog shopArray={shop.shopArray}/>
+          </div>
+      ) : (
+        <LoadingScreen />
+      )}
+    </div>
+  );
+}
+*/
