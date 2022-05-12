133b5bafc243c36e9b67c3c599ad82214297eef6
Chris Risden <christopherrisden@gmail.com>
2022-05-11 17:50:56 -0700

    used an async function to handle state if redux doesn't already have state set up for me on nested shop items, i.e /shop/19 without navigating through /shop

--- src/ExpandedShopItem.js
+++ src/ExpandedShopItem.js
@@ -2,36 +2,49 @@ import './expandedshopitem.css';
 import { useParams, useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import LoadingScreen from './LoadingScreen.js';
 
 const ExpandedShopItem = (props) => {
   const id = Number(useParams().id)
-  console.log(id, useParams())
   const shopItemObject = useSelector(state => state.shopData.shopArray[id])
 
   const [shopItem, setShopItem] = useState(shopItemObject)
-
+  const [loading, setLoading] = useState(true)
   console.log(shopItem)
   let navigate = useNavigate();
 
-  if (shopItem === undefined) {
-    const fetchedShopItem = undefinedShopItem()
-    setShopItem(fetchedShopItem)
-  }
+  useEffect(()=> {
+    if (shopItemObject === undefined) {
+      undefinedShopItem()
+    } else {
+      setShopItem(shopItemObject)
+      setLoading(false)
+    }
+    async function undefinedShopItem() {
+      const response = await axios.get(`https://fakestoreapi.com/products/${id+1}`)
+      const data = response.data;
+      console.log(data)
+      const fixedData = { ...data }
+      fixedData.description = data.description.charAt(0).toUpperCase() + data.description.slice(1);
+      fixedData.price = '$' + data.price.toFixed(2)
+      fixedData.img = data.image
+      console.log(fixedData)
+      setShopItem(fixedData)
+      setLoading(false)
+      return;
+    }
+  }, [shopItemObject, id])
 
-  async function undefinedShopItem() {
-    const response = await axios.get(`https://fakestoreapi.com/products/${id+1}`)
-    const data = response.data;
-    console.log(response, data)
-    return data;
-  }
+
+
   console.log(shopItem)
 
   //if shopItem is undefined, we should fetch the info, right now we are assuming that we navigated here through /shop/
 
   return (
     <div className="expanded-shop-item modal">
-      {shopItem !== undefined ? (
+      {loading === false ? (
         <div className="modal-content">
         <button onClick={() => navigate('/shop')}>Back to Shop</button>
         <img className="expanded-shop-item-image" src={shopItem.img} alt='{props.item.img}'/>
@@ -49,7 +62,7 @@ const ExpandedShopItem = (props) => {
         </div> */}
       </div>
       ) : (
-        <p>loading...</p>
+        <LoadingScreen />
       )}
   </div>
   );
