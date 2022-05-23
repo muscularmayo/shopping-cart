7292af30e0a00bdd199e4355eb9547e3efc93b29
Chris Risden <christopherrisden@gmail.com>
2022-05-22 19:55:18 -0700

    hover shopping cart works! gonna make it so it doesn't render when we're actually on the shopping cart

---
+++ src/HoverShoppingCart.js
@@ -0,0 +1,53 @@
+import './shoppingcart.css';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addToShoppingCart, handleInputChange } from './shoppingCartSlice.js'
+
+const HoverShoppingCart = () => {
+  const dispatch = useDispatch();
+  const shoppingCart = useSelector(state => state.shoppingCart)
+  const numberOfItems = shoppingCart.numberOfItems
+
+  const handleInput = (event) => {
+    console.log(event.target)
+    const info = {
+      id: event.target.id,
+      value: event.target.value
+    }
+    dispatch(handleInputChange(info))
+    //console.log(event.target.id)
+  }
+
+  // let numberOfItems = 0;
+  let priceTotal = 0;
+  shoppingCart.shoppingCartArray.forEach((e) => {
+    // numberOfItems += e.quantity;
+    let price = e.price.slice(1)
+    priceTotal += e.quantity*price
+  })
+  priceTotal = '$' + priceTotal.toFixed(2)
+  // priceTotal = '$' + priceTotal;
+  console.log(shoppingCart)
+
+
+
+  return (
+    <div className="shopping-cart">
+      <h3>Shopping Cart</h3>
+      <div>
+        Number of items in cart: {numberOfItems}
+      </div>
+      {shoppingCart.shoppingCartArray.map((item) => {
+        return (
+          <div>
+            <div>{item.name + ' ' + item.price}</div>
+            <input min={0} onChange={handleInput} id={item.id} type="number" value={item.quantity}/>
+          </div>
+        )
+      })}
+      <div id="priceTotal">Total: {priceTotal}</div>
+    </div>
+  );
+}
+
+export default HoverShoppingCart;
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,22 @@
 import './navbar.css';
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
+import ShoppingCart from './ShoppingCart.js'
+import HoverShoppingCart from './HoverShoppingCart.js'
+
 
 const NavBar = () => {
+  const numberOfItems = useSelector(state => state.shoppingCart.numberOfItems)
+  const shoppingCart = useSelector(state => state.shoppingCart)
+
+  const [style, setStyle] = useState({display: 'none'})
+
+  const location = useLocation();
+  const currentPath = location.pathname
+
+
+
   return (
     <div className="header">
       <ul className="nav-bar">
@@ -14,9 +29,35 @@ const NavBar = () => {
         <li>
           <Link to="/about">About</Link>
         </li>
+        {(currentPath !== '/shoppingcart') ? (
         <li>
-          <Link to="/shoppingcart">Shopping Cart</Link>
+          <Link to="/shoppingcart"
+                id="shoppingcart"
+                onMouseEnter={e => {
+                 setStyle({display: 'block'})
+                }}
+                onMouseLeave={e => {
+                  setStyle({display: 'none'})
+                }}>Shopping Cart ({numberOfItems})
+          </Link>
+          <div id="hover-cart"
+            style={style}
+            onMouseEnter={e => {
+                 setStyle({display: 'block'})
+                }}
+              onMouseLeave={e => {
+                setStyle({display: 'none'})
+              }}>
+            <HoverShoppingCart/>
+          </div>
         </li>
+        ) : (
+          <li>
+            <Link to="/shoppingcart"
+            />
+          </li>
+        )
+        }
       </ul>
     </div>
   );
